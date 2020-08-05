3941189ea697799ff51646216eeaac7f623b32f2
manfer993 <manfer93@gmail.com>
2020-08-05 11:42:05 -0500

    add final constants

--- src/ui/LoginUI.java
+++ src/ui/LoginUI.java
@@ -2,9 +2,9 @@ package ui;
 
 import org.openqa.selenium.By;
 
-public class LoginUI {
-	public static By username = By.id("user-name");
-	public static By password = By.id("password");
-	public static By loginButton = By.id("login-button");
-	public static By errorMessage = By.xpath("//h3[@data-test='error']");
+public final class LoginUI {
+	public static final By username = By.id("user-name");
+	public static final By password = By.id("password");
+	public static final By loginButton = By.id("login-button");
+	public static final By errorMessage = By.xpath("//h3[@data-test='error']");
 }
